f700b04b49853308ac287ca07a3cb16cc6d71888
Sugahara Toshihiro <48586314+tosh1hiro@users.noreply.github.com>
2023-05-12 02:13:01 +0900

    セクション4、スプレッド構文まで

--- src/index.js
+++ src/index.js
@@ -65,20 +65,84 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です。"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です。"));
 
 // アロー関数
-const func2 = (str) => {
-  return str;
-};
+// const func2 = (str) => {
+//   return str;
+// };
 // const func2 = (str) => str;
-console.log(func2("func2です。"));
+// console.log(func2("func2です。"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
 // const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "としひろ",
+//   age: 30
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}ですよ。`;
+// console.log(message2);
+
+// const myProfile = ["としひろ", 30];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2, 3];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2, num3) => console.log(num1 + num2 + num3);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// ※参照は引き継がれない
+// const arr6 = [...arr4];
+// arr6[0] = 1000;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// ※参照が引き継がれてしまう
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4);
